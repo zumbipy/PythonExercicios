6c8c2472ef90449feb304eea8aac3a15257d4821
ZumbiPy <zumbipy@gmail.com>
2018-02-05 16:49:49 -0200

    Update Ex 44

---
+++ EstruturaDeRepeticao/estruturaderepeticao-44.py
@@ -0,0 +1,63 @@
+# Telegram: @ZumbiPy __   _ ___
+#  /_  / __ ____ _  / /  (_) _ \__ __
+#   / /_/ // /  ' \/ _ \/ / ___/ // /
+#  /___/\_,_/_/_/_/_.__/_/_/   \_, /
+# E-mail: zumbipy@gmail.com   /___/ /dev - 04/02/2018
+"""
+44 - Em uma eleição presidencial existem quatro candidatos. Os votos são informados por meio de
+código.
+Os códigos utilizados são:
+1 , 2, 3, 4  - Votos para os respectivos candidatos
+(você deve montar a tabela ex: 1 - Jose/ 2- João/etc)
+5 - Voto Nulo
+6 - Voto em Branco
+Faça um programa que calcule e mostre:
+O total de votos para cada candidato;
+O total de votos nulos;
+O total de votos em branco;
+A percentagem de votos nulos sobre o total de votos;
+A percentagem de votos em branco sobre o total de votos. Para finalizar o conjunto de votos
+tem-se o valor zero.
+"""
+
+dic_candidatos = {
+    1: {"canditado": "joao", "voto": 0},
+    2: {"canditado": "maria", "voto": 0},
+    3: {"canditado": "josé", "voto": 0},
+    4: {"canditado": "nego ban", "voto": 0},
+    5: {"canditado": "Nulo", "voto": 0},
+    6: {"canditado": "Branco", "voto": 0}
+
+}
+total_votos = 0
+votos_nulos = 0
+votos_em_branco = 0
+
+
+def tabela_eleicao(dic):
+    print("""
++-----+------------+-------+
+| cod | Candidatos | Votos |
++-----+------------+-------+""")
+    for codigo in dic.keys():
+        votos = dic[codigo]['voto']
+        candidato = dic[codigo]['canditado']
+        print(f"|{codigo:^5}|{candidato.capitalize():<12}|{votos:>7}|")
+    print("+-----+------------+-------+")
+
+
+def votar(total_votos, dic):
+    while True:
+        codigo_voto = int(input("Para voto digite o codigo do Canditado ou 0 para sair: "))
+        if codigo_voto in dic.keys():
+            dic[codigo_voto]['voto'] += 1
+            print("Voto confirmando")
+            total_votos += 1
+        elif codigo_voto == 0:
+            return False
+
+
+if __name__ == '__main__':
+    tabela_eleicao(dic_candidatos)
+    votar(total_votos, dic_candidatos)
+    tabela_eleicao(dic_candidatos)
