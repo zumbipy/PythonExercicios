880282a677d364e8c727249563bf2428a46c3612
ZumbiPy <zumbipy@gmail.com>
2018-02-07 04:57:32 -0200

    Versao 1.5

--- automodreadememd.py
+++ automodreadememd.py
@@ -7,60 +7,61 @@
 """
     Entra em cada diretorio e conta quantos arquivos tem e atulizar o READEME.md
 """
-
 import os
+import os.path
+from string import ascii_uppercase
 
 
-def escrever_arquivo(nome_arquivo, texto):
+def criar_arquivo(nome_arquivo, texto):
     with open(nome_arquivo, "w") as arquivo:
         arquivo.write(texto)
 
 
-def dicionaro_de_diretorios(lista_ignora):
-    lista_diretorio = os.listdir()
-    dic_diretorio = {}
-    for diretorio in lista_diretorio:
-        if os.path.isdir(diretorio) and diretorio not in lista_ignora:
-            dic_diretorio[diretorio] = {'completos': 0}
-    return dic_diretorio
+def criar_dict_diretorio():
+
+    dict_diretorios = {diretorio: {} for diretorio in os.listdir() if os.path.isdir(diretorio)
+                       if "." not in diretorio}
+
+    for diretorio in dict_diretorios.keys():
+        dict_diretorios[diretorio]["Nome"] = separa_nome(diretorio)
+        dict_diretorios[diretorio]["Total"] = conta_arquivos(diretorio, ["py"])
+
+    return dict_diretorios
 
 
-def contar_arquivos(dicionario):
-    for chave in dicionario.keys():
-        total = len(os.listdir(chave))
-        dicionario[chave]['completos'] = total
-
-
-def nomeclatura_diretorio(dicionario):
-    nomes_arruma = dicionario.keys()
+def separa_nome(palavra):
     saida = ''
-    for nome in nomes_arruma:
-        for letra in nome:
-            if ord(letra) >= 65 and ord(letra) <= 90:
-                saida += f" {letra}"
-            else:
-                saida += letra
-        dicionario[nome]['Nome'] = saida.lstrip()
-        saida = ''
+    for letra in palavra:
+        if letra in ascii_uppercase:
+            saida += " " + letra
+        else:
+            saida += letra
+    return saida.lstrip()
+
+
+def conta_arquivos(local, tipos=["py"]):
+    total = 0
+    for arquivo in os.listdir(local):
+        if arquivo.split(".")[1] in tipos:
+            total += 1
+    return total
+
+
+def template_texto(texto, dict_diretorios):
+    saida = texto
+    for chave in dict_diretorios:
+        saida += f'**{dict_diretorios[chave]["Nome"]}** | {dict_diretorios[chave]["Total"]}\n'
+    return saida
 
 
 if __name__ == '__main__':
-    lista_ignora = [".git"]
-    diretorios = dicionaro_de_diretorios(lista_ignora)
-    nomeclatura_diretorio(diretorios)
-    contar_arquivos(diretorios)
-
     texto = """# **Python Exercícios**
 > Treinando Algoritmo e Lógica de Programação com os exercícios disponíveis em: https://wiki.python.org.br/ListaDeExercicios
 
 # Lista de exercícios:
 
  Tipo de exercício | Concluido
-----------------------------|:----------------:
-"""
-    for linha in diretorios.keys():
-        Nome_d = diretorios[linha]["Nome"]
-        finalizados = diretorios[linha]["completos"]
-        texto += f"**{Nome_d}** | {finalizados}\n"
+----------------------------|:----------------:\n"""
 
-    escrever_arquivo("README.md", texto)
+    dic = criar_dict_diretorio()
+    criar_arquivo("README.md", template_texto(texto, dic))
