9c67e996794276a9acd02956b81b6d570706d07b
zumbipy <zumbipy@gmail.com>
2018-02-17 20:47:13 -0200

    Modificação

--- EstruturaDeRepeticao/estruturaderepeticao-49.py
+++ EstruturaDeRepeticao/estruturaderepeticao-49.py
@@ -5,31 +5,35 @@
 # E-mail: zumbipy@gmail.com   /___//dev - 17/02/2018
 """
 49 - Faça um programa que mostre os n termos da Série a seguir:
-  S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m. 
+S = 1/1 + 2/3 + 3/5 + 4/7 + 5/9 + ... + n/m.
+Imprima no final a soma da série.
 """
 
 
 def n_termnos(numero):
     lista_termos = []
-    a, b, ta, tb = 1, 1, 0, 0
+    a, b = 1, 1
     for n in range(1, (numero + 1)):
-        ta, tb = (ta + a), (tb + b)
-        lista_termos.append(f"{a}/{b}")
+        lista_termos.append([a, b])
         a, b = (a + 1), (b + 2)
-    lista_termos.append(f'{ta}/{tb}')
     return lista_termos
 
 
 def imprimir(lista):
     saida = "S ="
     for item in lista:
-        if item == lista[-1]:
-            saida += f' = {item} '
+        a, b = item
+        if item != lista[-1]:
+            saida += f' {a}/{b} +'
         else:
-            saida += f' {item}'
-            if item != lista[-2]:
-                saida += " +"
-    print(saida)
+            saida += f' {a}/{b}'
+        total_item = soma_termos(lista)
+    print(f'{saida} = {total_item:.2f}')
 
 
-imprimir(n_termnos(5))
+def soma_termos(lista):
+    return sum([a / b for a, b in lista])
+
+
+if __name__ == '__main__':
+    imprimir(n_termnos(5))
