b336b7e8b4d481c6a190cc61412de12db60a61ec
zumbipy <zumbipy@gmail.com>
2018-03-18 12:39:18 -0300

    Update Classes 1 e 2

---
+++ ExerciciosClasses/ExerciciosClasses-01.py
@@ -0,0 +1,24 @@
+# Telegram: @ZumbiPy __   _ ___
+#  /_  / __ ____ _  / /  (_) _ \__ __
+#   / /_/ // /  ' \/ _ \/ / ___/ // /
+#  /___/\_,_/_/_/_/_.__/_/_/   \_, /
+# E-mail: zumbipy@gmail.com   /___/ /dev 13/03/2018
+"""
+01 - Classe Bola: Crie uma classe que modele uma bola:
+
+Atributos: Cor, circunferência, material
+Métodos: trocaCor e mostraCor
+"""
+
+
+class Bola(object):
+    def __init__(self, cor, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+
+    def trocar_cor(self):
+        self.cor = input("Digite nova cor: ")
+
+    def monstrar_cor(self):
+        return self.cor
---
+++ ExerciciosClasses/ExerciciosClasses-02.py
@@ -0,0 +1,21 @@
+# Telegram: @ZumbiPy __   _ ___
+#  /_  / __ ____ _  / /  (_) _ \__ __
+#   / /_/ // /  ' \/ _ \/ / ___/ // /
+#  /___/\_,_/_/_/_/_.__/_/_/   \_, /
+# E-mail: zumbipy@gmail.com   /___/ /dev 13/03/2018
+"""
+02 - Classe Quadrado: Crie uma classe que modele um quadrado:
+
+Atributos: Tamanho do lado
+Métodos: Mudar valor do Lado, Retornar valor do Lado e calcular Área; 
+"""
+
+
+class Quadrado(object):
+    def __init__(self, tamano_do_lado=10):
+        self.lados = tamano_do_lado
+    
+    def mudao_valor_lados(self, numero):
+        self.lados = numero
+        self.area = numero * 2
+        return self.lados, self.area
