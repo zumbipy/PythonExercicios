0e6cfbb2cd74f5f6fdba971239359771aa0cce2a
zumbipy <zumbipy@gmail.com>
2018-03-20 01:02:09 -0300

    Update class 4

---
+++ ExerciciosClasses/ExerciciosClasses-04.py
@@ -0,0 +1,13 @@
+# Telegram: @ZumbiPy __   _ ___
+#  /_  / __ ____ _  / /  (_) _ \__ __
+#   / /_/ // /  ' \/ _ \/ / ___/ // /
+#  /___/\_,_/_/_/_/_.__/_/_/   \_, /
+# E-mail: zumbipy@gmail.com   /___// dev 20/03/2018
+"""
+04 - Classe Pessoa: Crie uma classe que modele uma pessoa:
+
+Atributos: nome, idade, peso e altura
+Métodos: Envelhercer, engordar, emagrecer, crescer. 
+Obs: Por padrão, a cada ano que nossa pessoa envelhece,
+sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
+"""
