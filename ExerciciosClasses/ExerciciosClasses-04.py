0633d0e4884bb53ec9408db9ab9d2be9d122395d
zumbipy <zumbipy@gmail.com>
2018-03-23 00:49:43 -0300

    Pep-8

--- ExerciciosClasses/ExerciciosClasses-04.py
+++ ExerciciosClasses/ExerciciosClasses-04.py
@@ -7,20 +7,19 @@
 04 - Classe Pessoa: Crie uma classe que modele uma pessoa:
 
 Atributos: nome, idade, peso e altura
-Métodos: Envelhercer, engordar, emagrecer, crescer. 
+Métodos: Envelhercer, engordar, emagrecer, crescer.
 Obs: Por padrão, a cada ano que nossa pessoa envelhece,
 sendo a idade dela menor que 21 anos, ela deve crescer 0,5 cm.
 """
 
 
 class Pessoa(object):
-    
     def __init__(self, nome, idade, peso, altura):
         self.idade = idade
         self.nome = nome
         self.peso = peso
         self.altura = altura
-    
+
     def __repr__py(self):
         return f"Nome: {self.nome}\nIdade:{self.idade}\nPeso:{self.peso}\nAltura:{self.altura}"
 
@@ -30,7 +29,7 @@ class Pessoa(object):
             return False
         else:
             return True
-    
+
     def envelhercer(self, idade):
         if self.positivo(idade):
             if idade < 21:
@@ -38,25 +37,25 @@ class Pessoa(object):
                 self.idade += idade
             else:
                 self.idade += idade
-            
+
     def engordar(self, peso):
         if self.positivo(peso):
             print("Valor Invalido!!!")
         else:
             self.peso += peso
-    
+
     def emagrecer(self, peso):
         if self.positivo(peso):
             print("Valor Invalido!!!")
         else:
             self.peso -= peso
-    
+
     def crescer(self, altura):
         if self.positivo(altura):
             self.altura += altura
         else:
             print("Valor Invalido!!!")
-            
+
 
 pynoob = Pessoa("Noob", 10, 49, 130)
 
