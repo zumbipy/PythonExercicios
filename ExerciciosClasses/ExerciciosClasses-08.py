38f3fe92c17754f2665db7c21264d79b2fdab0f6
zumbipy <zumbipy@gmail.com>
2018-03-31 16:08:47 -0300

    Pep8

--- ExerciciosClasses/ExerciciosClasses-08.py
+++ ExerciciosClasses/ExerciciosClasses-08.py
@@ -16,27 +16,25 @@ class Macaco(object):
     def __init__(self, nome, estomago):
         self.nome = nome
         self.estomago = estomago
-    
+
     def __repr__(self):
         return f"Nome: {self.nome}\nEstomago: {self.estomago}"
-    
+
     def comer(self, comida):
-        dict_comida = {"banana":100, "maça":50, "formiga":10}
+        dict_comida = {"banana": 100, "maça": 50, "formiga": 10}
 
         if comida in dict_comida.keys():
             self.estomago += dict_comida[comida]
         else:
             print("Valor Inválido")
             print("Macaco pode come ... Banana, Maça ou Formiga")
-    
+
     def ver_estomago(self):
         return self.estomago
-    
+
     def digerir(self):
         self.estomago -= 20
         return self.estomago
-    
+
 macaco1 = Macaco("PyNoob", 78)
 macaco2 = Macaco("PyZumbi", 20)
-
-
